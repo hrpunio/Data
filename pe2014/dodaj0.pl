572e2e536bf62b6ead416a05a36b0dc504a9aed1
tomasz przechlewski <rudolf.von.ems@gmail.com>
2016-04-18 19:22:55 +0200

    aktualizacja

--- pe2014/dodaj0.pl
+++ pe2014/dodaj0.pl
@@ -3,7 +3,7 @@ while (<>) { chomp();
 
    $teryt = $tmp[1];
    if (length($teryt) < 6) { $teryt = "0" . $teryt  }
-   #print "$tmp[0];$teryt;$tmp[2];$tmp[3];$tmp[4];$tmp[5];$tmp[6];$tmp[7];$tmp[8];$tmp[9];$tmp[10]\n";
-   print "$tmp[0];$teryt;$tmp[2];$tmp[3];$tmp[4];$tmp[5];$tmp[6]\n";
+   print "$tmp[0];$teryt;$tmp[2];$tmp[3];$tmp[4];$tmp[5];$tmp[6];$tmp[7];$tmp[8];$tmp[9];$tmp[10]\n";
+   ##print "$tmp[0];$teryt;$tmp[2];$tmp[3];$tmp[4];$tmp[5];$tmp[6]\n";
 
 }
--- pe2014/freq.R
+++ pe2014/freq.R
@@ -8,11 +8,19 @@ require(ggplot2)
 
 d <- read.csv("komisje-frekwencja14_15.csv", sep = ';',  header=T, na.string="NA");
 str(d)
+# Usuń zagraniczne (nietypowe):
+d <- subset (d, ( teryt != 149901 ));
+str(d)
 
 ggplot(d, aes(x = freq)) +
   geom_point(aes(y = freq15), colour = 'blue') +
   xlab(label="freq 2014") +
   ylab(label="freq 2015")
+
+ggplot(d, aes(x = pgnw)) +
+  geom_point(aes(y = pgnw15), colour = 'blue') +
+  xlab(label="nonvalid 2014 (%)") +
+  ylab(label="nonvalid 2015 (%)")
 
 #
 d <- subset (d, ( lkw > 20 & lkw15 > 20 ));
@@ -23,9 +31,20 @@ ggplot(d, aes(x = freq)) +
   xlab(label="freq 2014") +
   ylab(label="freq 2015")
 
-d <- subset (d, ( freq > 40 & freq15 > 40 ));
+d <- subset (d, ( freq > 60 & freq15 > 60 ));
 str(d)
 ggplot(d, aes(x = freq)) +
   geom_point(aes(y = freq15), colour = 'blue') +
   xlab(label="freq 2014") +
   ylab(label="freq 2015")
+
+d;
+
+ggplot(d, aes(x = pgnw)) +
+  geom_point(aes(y = pgnw15), colour = 'blue') +
+  xlab(label="nonvalid 2014 (%)") +
+  ylab(label="nonvalid 2015 (%)")
+
+d <- subset (d, !grepl("Dom pomocy|Domu Pomocy|Areszt", adres, ignore.case = TRUE));
+
+d;
