1b865eb41817f6cffabc38bbbe75ec33551e0b33
tomasz przechlewski <rudolf.von.ems@gmail.com>
2016-04-18 19:22:55 +0200

    aktualizacja

--- pe2014/joincvs14_15.pl
+++ pe2014/joincvs14_15.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 #
-open (P14, "komisje-frekwencja.csv");
-open (P15, "../sejm2015/komisje-frekwencja2015.csv");
+open (P14, "komisje-frekwencja.csv") || die "Cannot open 1\n";
+open (P15, "../sejm2015/komisje-frekwencja2015.csv") || die "Cannot open 2\n";
 
 while (<P14>) { chomp(); @tmp=split (/;/, $_); $L{"$tmp[1]_$tmp[2]"} = $_; }
 
@@ -10,7 +10,7 @@ while (<P15>) {
   $id = "$tmp[1]_$tmp[2]";
 
   if (exists $L{$id} ) {
-     print "$L{$id};$tmp[4];$tmp[5];$tmp[6];$tmp[7];$tmp[8];$tmp[9];$tmp[10]\n";
+     print "$L{$id};$tmp[4];$tmp[5];$tmp[6];$tmp[7];$tmp[8];$tmp[9];$tmp[10];$tmp[3]\n";
   } else {
     warn "NO $id found\n!";
   }
