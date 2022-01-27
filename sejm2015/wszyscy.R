99c809a61c0eaf60e33d4d782ac21e58744e9f6a
tomasz przechlewski <rudolf.von.ems@gmail.com>
2022-01-27 17:56:00 +0100

    przeniesione

--- sejm2015/wszyscy.R
+++
@@ -1,4 +0,0 @@
-kandydaci <- read.csv("kandydaci_obwody_wyniki.csv", sep = ';',  header=T, na.string="NA");
-str(kandydaci);
-
-aggregate (kandydaci$glosy, list(Numer = kandydaci$komitet), na.rm=TRUE, sum);
--- ws2014/convertpdf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-B=`basename $1.pdf`
-
-convert -density 300 $1 -background white -alpha remove  ${B}_%02d.png
