518a701ba07eac26593c583fefa642bb241dd53d
tomasz przechlewski <rudolf.von.ems@gmail.com>
2022-01-27 18:02:16 +0100

    ak

--- pkw/kandydaci.R
+++
@@ -1,8 +0,0 @@
-#
-kandydaci <- read.csv("kandydaci_2011.csv", sep = ';',  header=T, na.string="NA");
-
-aggregate (kandydaci$glosy, list(Numer = kandydaci$numer), fivenum)
-
-kandydaci <- subset (kandydaci, subset=(numer < 10 ));
-kandydaci <- subset (kandydaci, subset=(glosy < 200000));
-boxplot (glosy ~ numer, kandydaci, xlab = "Nr", ylab = "L.glosów", col = "yellow")
