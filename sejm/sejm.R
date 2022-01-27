a742b8a72a920b69dda3b011524f448d1b5bbedd
tomasz przechlewski <rudolf.von.ems@gmail.com>
2022-01-27 18:02:16 +0100

    ak

--- sejm/sejm.R
+++
@@ -1,7 +0,0 @@
-poslowie <- read.csv("Sejm1-8.csv", sep = ';',  header=T, na.string="NA");
-
-boxplot (wiek ~ kadencja, poslowie, xlab = "Kadencja", ylab = "Wiek", col='yellow')
-mtext(text="https://github.com/hrpunio/Data/tree/master/sejm", 4, cex=0.7)
-
-aggregate (poslowie$wiek, list(Kadencja = poslowie$kadencja), fivenum)
-aggregate (poslowie$wiek, list(Kadencja = poslowie$kadencja), na.rm=TRUE, mean)
